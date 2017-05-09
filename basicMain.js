b3bb189e0263f737c12be833bd89096055cd7932
joshua wood <joshua wood>
2017-05-09 16:45:20 -0400

    fixed minor bug

--- basicMain.js
+++ basicMain.js
@@ -23,12 +23,13 @@ function startGame() {
 
 function endGame(score) {
 
-  console.log("Game Over!");
-  console.log("Your score is:", score);
+  console.log("GAME OVER!");
+  console.log("Your score is: ", score);
   inquirer.prompt([{
+
     type: "input",
     name: "text",
-    message: "Play again?"
+    message: "TYPE [y] TO PLAY AGAIN or PRESS ENTER TO QUIT"
 
   }]).then(function(answer) {
 
@@ -37,48 +38,49 @@ function endGame(score) {
       startGame();
     } 
     else {
-      // Otherwise the game ends here since we aren't calling any other functions
-      console.log("Thanks for playing!");
-      console.log("Goodbye!");
+    
+      console.log("PLAY AGAIN SOON!");
     }
   });
 }
 
 function playRound(currentScore, cardArray, currentIndex) {
-  // If we have't gone through all the cards, ask the user the next question
+
   if (currentIndex < cardArray.length) {
     promptUser(cardArray, currentIndex, currentScore);
   }
-  // Otherwise end the game
+
   else {
     endGame(currentScore);
   }
 }
 
 function promptUser(cardArray, currentIndex, currentScore) {
-  // Storing our current card in the card variable
+
   var card = cardArray[currentIndex];
-  // Show the user the front of the card, ask for an answer
+
   inquirer.prompt([{
+
     type: "input",
     name: "text",
-    message: card.front + "\nAnswer:"
+    message: card.front + "\nANSWER: "
+
   }]).then(function(answer) {
-    // Checking to see if their answer was correct, regardless of casing
+
     if (answer.text.trim().toLowerCase() === card.back.trim().toLowerCase()) {
-      // If the user is correct, increase the score by 1
+   
       currentScore++;
-      console.log("You are correct!");
+      console.log("CORRECT!");
     } 
     else {
-      // Otherwise let them know they were incorrect
-      console.log("Incorrect! The correct answer is '" + card.back + "'.");
+
+      console.log("INCORRECT! YOU SHOULD HAVE SAID '" + card.back + "'.");
     }
-    // Increase our current card index
+
     currentIndex++;
-    // Just a seperator
+
     console.log("-------------------------");
-    // Play the next round with our updated score and card index
+
     playRound(currentScore, cardArray, currentIndex);
   });
 }
